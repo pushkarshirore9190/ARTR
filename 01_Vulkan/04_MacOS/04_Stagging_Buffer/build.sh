f285556966caa72608df8507fed45b8e8db1857c
pushkarshirore9190 <pushkarshirore2020@gmail.com>
2026-06-12 02:02:28 +0530

    StaggingBuffer done in Mac

---
+++ 01_Vulkan/04_MacOS/04_Stagging_Buffer/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rm -rf VK.app
+
+rm -rf log.txt
+
+mkdir -p VK.app/Contents/MacOS
+
+clang++ -Wno-deprecated-declarations -arch arm64 -o VK.app/Contents/MacOS/VK -I$HOME/VulkanSDK/Vulkan/MacOS/include -L$HOME/VulkanSDK/Vulkan/MacOS/lib -F$HOME/VulkanSDK/Vulkan/MacOS/Frameworks -rpath $HOME/VulkanSDK/Vulkan/MacOS/Frameworks VK.m -framework Foundation -framework Cocoa -framework QuartzCore -framework vulkan
+
+
+
+
+
+
+
+
+
+
