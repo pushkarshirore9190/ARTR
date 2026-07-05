8be6a4c50510a5f1b55381032ddc5f94731d90f3
pushkarshirore9190 <pushkarshirore2020@gmail.com>
2026-07-05 14:51:49 +0530

    Two 3D BW Shapes Done in iOS

---
+++ 01_Vulkan/05_IOS/17_TwoShapesBW/Template/Template/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Template
+//
+//  Created by Pushkar Rajendra Shirore on 17/06/26.
+//
+
+#import <UIKit/UIKey.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
+
