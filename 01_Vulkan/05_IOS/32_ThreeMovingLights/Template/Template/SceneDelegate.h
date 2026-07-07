198833b505b5dfeb8681eaa597279d99ce271f19
pushkarshirore9190 <pushkarshirore2020@gmail.com>
2026-07-08 00:43:13 +0530

    ThreeMovingLights Done in iOS

---
+++ 01_Vulkan/05_IOS/32_ThreeMovingLights/Template/Template/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Template
+//
+//  Created by Pushkar Rajendra Shirore on 17/06/26.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@end
+
+
